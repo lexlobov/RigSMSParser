ab31f6d09d139f7e4866d8766b07e36e29a3cdf1
Alex Lobov <alobov@issart.com>
2022-11-29 12:08:42 +0700

    Дописал README

--- src/main/java/com/issart/rig/parsers/Parser.java
+++ src/main/java/com/issart/rig/parsers/Parser.java
@@ -14,14 +14,20 @@ public class Parser {
     }
 
     protected String getHtml(String phone) throws IOException {
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        Request request = new Request.Builder()
-                .url(linkBuilder(phone))
-                .method("GET", null)
-                .addHeader("Cookie", "SMSSId=aRTlVW5zgg5FMWXwUSH626h6TSsT8RF%2BE07297KzGjr41UVZ3CzcaXMfLanj8Jsg7yZ1gmpCsoa9zs%2BzNAHGjT5PjA")
-                .build();
-        Response response = client.newCall(request).execute();
+        Response response = null;
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            Request request = new Request.Builder()
+                    .url(linkBuilder(phone))
+                    .method("GET", null)
+                    .addHeader("Cookie", "SMSSId=aRTlVW5zgg5FMWXwUSH626h6TSsT8RF%2BE07297KzGjr41UVZ3CzcaXMfLanj8Jsg7yZ1gmpCsoa9zs%2BzNAHGjT5PjA")
+                    .build();
+            response = client.newCall(request).execute();
+        } catch (Exception e){
+            System.out.println("Wasn't able to reach https://receive-sms.cc/. Probably VPN or Proxy connection is needed");
+            return "false";
+        }
         if(!response.isSuccessful()){
             return "false";
         } else {
